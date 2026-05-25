5b6ac1b3ac8f6376a7826e486dbf736853938fab
Weerapat <68030271@kmitl.ac.th>
2026-05-25 12:45:16 +0700

    Add SSL disable-verify option for Railway MySQL TCP proxy

--- config/database.php
+++ config/database.php
@@ -16,9 +16,10 @@ $charset  = 'utf8mb4';
 
 $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=$charset";
 $options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
+    PDO::ATTR_ERRMODE                  => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE       => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES         => false,
+    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
 ];
 
 try {
